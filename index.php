c81c5609a1497392be4b2e9140af596ed4e76870
neagle-spec <neagleaaron@hotmail.com>
2023-02-20 17:02:27 -0500

    updated styling

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
+// Require the necessary files
+require_once('vendor/autoload.php');
+
+//Create an instance of the base class
+$f3 = Base::instance();
+
+//Create an instance of the Controller class
+//$con = new Controller($f3);
+
+//Define a default route
+$f3->route('GET /', function () {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
