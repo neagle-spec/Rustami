870c8e1a96f35167afb37ef3e4c45a07a502ba29
neagle-spec <neagleaaron@hotmail.com>
2023-03-02 02:37:32 -0500

    home.php file created by Fauzia

---
+++ views/home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Rustami Auto Maintenance</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="../js/script.js"></script>
+    <link rel="stylesheet" href="../css/styles.css">
+
+</head>
+<body>
+
+<?php include("includes/header.php") ?>
+
+<div class="jumbotron text-center">
+    <h1>Rustami Auto Maintenance</h1>
+    <p>Best Auto Maintenance in town</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <p>
+                We are an auto workshop dedicated to providing exceptional service and expert repairs for all types of vehicles. Our team of experienced mechanics is committed to ensuring that your vehicle is safe, reliable, and running at its best. At our workshop, we use the latest diagnostic tools and techniques to identify and repair any issues your vehicle may be experiencing. We offer a wide range of services, including routine maintenance, brake repairs, engine diagnostics, and much more. We understand that your vehicle is an important investment, which is why we treat it with the utmost care and attention to detail. Our goal is to ensure that you can drive with confidence, knowing that your vehicle is in top condition. At our auto workshop, we believe in providing a transparent and honest service. We will always explain any necessary repairs or maintenance, and we will never recommend services that we do not need. We are committed to building lasting relationships with our customers, based on trust and respect. If you're looking for a reliable, trustworthy, and cheap price auto workshop, look no further. Contact us today to schedule an appointment and experience the difference our team can make for your vehicle.
+            </p>
+            <p>
+                ما یک کارگاه خودرو هستیم که به ارائه خدمات استثنایی و تعمیرات تخصصی برای انواع خودروها اختصاص دارد. تیم مکانیک باتجربه ما متعهد است که مطمئن شود وسیله نقلیه شما ایمن، قابل اعتماد و در بهترین حالت کار می کند. ما در کارگاه خود از جدیدترین ابزارها و تکنیک های تشخیصی برای شناسایی و تعمیر هر گونه مشکلی که ممکن است وسیله نقلیه شما با آن مواجه شود، استفاده می کنیم. ما طیف گسترده ای از خدمات، از جمله تعمیر و نگهداری معمول، تعمیرات ترمز، عیب یابی موتور، و موارد دیگر را ارائه می دهیم. ما درک می کنیم که وسیله نقلیه شما یک سرمایه گذاری مهم است، به همین دلیل است که ما با نهایت دقت و توجه به جزئیات با آن رفتار می کنیم. هدف ما این است که اطمینان حاصل کنیم که می توانید با اطمینان خاطر رانندگی کنید، زیرا می دانید که وسیله نقلیه شما در بهترین شرایط است. ما در کارگاه خودرو خود به ارائه خدمات شفاف و صادقانه اعتقاد داریم. ما همیشه هر گونه تعمیر یا نگهداری لازم را توضیح خواهیم داد و هرگز خدماتی را که به آن نیاز نداریم توصیه نمی کنیم. ما متعهد به ایجاد روابط پایدار با مشتریان خود بر اساس اعتماد و احترام هستیم. اگر به دنبال یک کارگاه خودرو قابل اعتماد، قابل اعتماد و ارزان هستید، بیشتر از این دنبال نکنید. برای تعیین قرار ملاقات و تجربه تفاوتی که تیم ما می تواند برای وسیله نقلیه شما ایجاد کند، همین امروز با ما تماس بگیرید.
+            </p>
+        </div>
+
+    </div>
+
+
+    <div class="row">
+        <div class="wrapper">
+            <a href="#"><div class="bg-ico" id="facebook"><i class="fab fa-facebook social  facebook fa-3x"></i></div></a>
+            <a href="#"><div class="bg-ico" id="pinterest"><i class="fab fa-pinterest social  pinterest fa-3x"></i></div></a>
+            <a href="#"><div class="bg-ico" id="twitter"><i class="fab fa-twitter social  twitter fa-3x"></i></div></a>
+            <a href="#"><div class="bg-ico" id="instagram"><i class="fab fa-instagram social  instagram fa-3x"></i></div></a>
+            <a href="#"><div class="bg-ico" id="tumblr"><i class="fab fa-tumblr social  tumblr fa-3x"></i></div></a>
+            <a href="#"><div class="bg-ico" id="whatsapp"><i class="fab fa-whatsapp social  whatsapp fa-3x"></i></div></a>
+            <a href="#"><div class="bg-ico" id="youtube"><i class="fab fa-youtube social  youtube fa-3x"></i></div></a>
+        </div>
+    </div>
+
+</div>
+
+<script src="../js/script.js"></script>
+
+</body>
+</html>
