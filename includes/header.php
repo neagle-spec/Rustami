de8dd081e80d73769e1ff35902dbaa60125bff79
neagle-spec <neagleaaron@hotmail.com>
2023-03-02 02:36:09 -0500

    header file created by Fauzia

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<nav class="navbar">
+  <div class="container">
+    <div class="logo"><img src="images/logo.png" alt="LOGO"></div>
+    <div class="push-left">
+        <button id="menu-toggler" data-class="menu-active" class="hamburger">
+        <span class="hamburger-line hamburger-line-top"></span>
+        <span class="hamburger-line hamburger-line-middle"></span>
+        <span class="hamburger-line hamburger-line-bottom"></span>
+        </button>
+
+        <!--  Menu compatible with wp_nav_menu  -->
+        <ul id="primary-menu" class="menu nav-menu">
+        <li class="menu-item current-menu-item"><a class="nav__link"  href="home.php">Home</a></li>
+        <li class="menu-item current-menu-item"><a class="nav__link"  href="gallery.php">Gallery</a></li>
+        <li class="menu-item dropdown"><a class="nav__link"  href="#">Services</a>
+            <ul class="sub-nav" >
+            <li><a class="sub-nav__link" href="#">Brake repair</a></li>
+            <li><a class="sub-nav__link" href="#">Tire repair</a></li>
+            <li><a class="sub-nav__link" href="#">Engine repair</a></li>
+            <li><a class="sub-nav__link" href="#">Battery repair</a></li>
+            <li><a class="sub-nav__link" href="#">Steering repair</a></li>
+            </ul>
+
+        </li>
+        <li class="menu-item "><a class="nav__link"  href="book.php">Book appointments</a>
+        
+        </li>
+        </ul>
+
+
+    </div>
+  </div>
+</nav>
