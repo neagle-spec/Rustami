118c72ecd23dd949d7d781c3343842ca57b3c9c3
neagle-spec <neagleaaron@hotmail.com>
2023-03-02 02:35:23 -0500

    gallery.php page created by Fauzia

---
+++ views/gallery.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Rustami Auto Maintenance</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script src="../js/script.js"></script>
+  <link rel="stylesheet" href="../css/styles.css">
+
+</head>
+<body>
+
+<?php include('includes/header.php') ?>
+
+
+  
+<div class="container">
+  <div class="row my-4">
+  	<div class="col-sm-4 my-4">
+		<img src="../images/1%20(1).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(2).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(3).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(4).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(5).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(6).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(7).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(8).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(9).jpg" width="100%"/>
+	</div>
+    <div class="col-sm-4 my-4 ">
+		<img src="../images/1%20(10).jpg" width="100%"/>
+	</div>
+  </div>
+</div>
+
+</body>
+</html>
